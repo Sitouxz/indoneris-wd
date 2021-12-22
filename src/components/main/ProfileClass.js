746c45cc90557492d6330f19c820b875ca66bc40
MG177 <mg.terok77@gmail.com>
2021-12-22 11:35:35 +0800

    save

---
+++ src/components/main/ProfileClass.js
@@ -0,0 +1,21 @@
+import Class from "./Class";
+
+export default function ProfileClass() {
+    return (
+        <section
+            className="
+                container
+                d-flex
+                flex-wrap
+                justify-content-center
+                align-items-center
+                gap-3
+                mb-5
+                mt-3
+            ">
+            <Class />
+            <Class />
+            <Class />
+        </section>
+    );
+}
